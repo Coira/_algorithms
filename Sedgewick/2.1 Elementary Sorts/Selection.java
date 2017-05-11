b5d21a35d76818706c6ea0c34c1dbd0b67801f43
Coira <coira.ells@gmail.com>
2017-05-11 23:42:51 +0100

    Mergesort

--- Sedgewick/2.1 Elementary Sorts/Selection.java
+++ Sedgewick/2.1 Elementary Sorts/Selection.java
@@ -4,7 +4,7 @@ public class Selection {
     public static void sort(Comparable[] a) {
         int N = a.length;
         for (int i = 0; i < N; i++) {
-            // Exhange a[i] with smallest entry in a[i+1..N).
+            // Exchange a[i] with smallest entry in a[i+1..N).
             int min = i;
             for (int j = i+1; j < N; j++) {
                 if (less(a[j], a[min])) { min = j; }
