fd2821e54f9120218620a4fba07f578d48194f9c
Coira <coira.ells@gmail.com>
2017-05-31 16:14:35 +0100

    Notes on quicksort chapter

---
+++ Sedgewick/2.3 Quicksort/Quick.java
@@ -0,0 +1,30 @@
+public class Quick {
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);  // Eliminate dependence on input
+        sort(a, 0, a.length-1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        // Partition into a[lo..i-1], a[i], a[i+1..hi].
+        int i = lo, j = hi+1;  // left and right scan indices
+        Comparable v = a[lo];  // partitioning item
+
+        while (true) {
+            // Scan right, scan left, check for scan complete, and exchange
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+}
+
