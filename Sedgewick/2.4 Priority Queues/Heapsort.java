6c44b5efbf1ac1c8c371e799cab35702e89d06df
Coira <coira.ells@gmail.com>
2017-07-16 23:21:36 +0100

    Notes on priority queues

--- Sedgewick/2.4 Priority Queues/Heapsort.java
+++ Sedgewick/2.4 Priority Queues/Heapsort.java
@@ -1,5 +1,27 @@
 public class Heapsort {
-    public static void sort(Comparalbe[] a) {
+    private static void exch(Comparable[] a, int i, int j) {
+        i--; j--;
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void sink(Comparable[] a, int k, int N) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && less(a, j, j+1)) j++;
+            if (!less(a, k, j)) break;
+            exch(a, k, j);
+            k = j;
+        }
+    }
+
+    private static boolean less(Comparable[] a, int i, int j) {
+        i--; j--;
+        return a[i].compareTo(a[j]) < 0;
+    }
+    
+    public static void sort(Comparable[] a) {
         int N = a.length;
 
         for (int k = N/2; k >= 1; k--)
@@ -9,5 +31,18 @@ public class Heapsort {
             exch(a, 1, N--);
             sink(a, 1, N);
         }
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.print();
+    }
+    
+    public static void main(String[] args) {
+        String[] a = "25138".split("");
+        sort(a);
+        show(a);
     }
 }
--- Sedgewick/2.4 Priority Queues/MaxPQ.java
+++ Sedgewick/2.4 Priority Queues/MaxPQ.java
@@ -20,27 +20,42 @@ public class MaxPQ<Key extends Comparable<Key>> {
 
     public void insert(Key v) {
         pq[++N] = v;
-        swim(N0);
+        swim(N);
     }
 
     public Key delMax() {
         Key max = pq[1];     // Retrieve max key from top
         exch(1, N--);        // Exch with last item.
-        pq(N+1] = null;      // Avoid loitering.
+        pq[N+1] = null;      // Avoid loitering.
         sink(1);             // Restore heap property.
+        return max;
     }
 
-    // TODO fill these in
     private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
     }
 
     private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
     }
 
     private void swim(int k) {
+        while (k > 1 && less(k/2, k)) {
+            exch(k/2, k);
+            k = k/2;
+        }
     }
 
     private void sink(int k) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && less(j, j+1)) j++;
+            if (!less(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
     }
 }
 
